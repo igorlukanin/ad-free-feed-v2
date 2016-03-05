e9298301fbc7232b8eb973a58efef2e06130ab1c
Igor Lukanin <mail@igor.lukanin.name>
2016-03-05 11:25:02 +0500

    App skeleton

---
+++ src/app.js
@@ -0,0 +1,13 @@
+var log = require('./util/log');
+
+
+process.on('uncaughtException', function(err) {
+    console.error(err.stack);
+});
+
+require('promise/lib/rejection-tracking').enable({
+    allRejections: true
+});
+
+
+log.appState('main', 'started');
---
+++ src/util/log.js
@@ -0,0 +1,19 @@
+var debug = require('console-debug');
+
+
+var console = new debug({
+    uncaughtExceptionCatch: true,
+    logToFile:              true,
+    colors:                 true,
+    consoleFilter:          [],
+    logFilter:              []
+});
+
+var logAppState = function(app, message) {
+    console.info('Application ' + app + ': ' + message);
+};
+
+
+module.exports = {
+    appState: logAppState
+};
